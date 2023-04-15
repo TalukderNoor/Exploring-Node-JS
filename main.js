459f2a2d47e14a3a037f0d3d3b006558c7592cd1
TalukderNoor <119832596+TalukderNoor@users.noreply.github.com>
2023-04-16 02:40:12 +0600

    Request Respond Model

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 var http = require("http");
 
-//first node server code
+//------first node server code
+/*
 var server = http.createServer(function (req, res) {
   res.end("Hello World!");
+});
+
+server.listen(5050);
+console.log("Server run succesfully");
+*/
+
+//------Request Respond Model
+
+var server = http.createServer(function (req, res) {
+    if (req.url == "/") {
+        res.writeHead(200, {
+            "Content-Type": "text-html",
+        });
+        res.write("<h1>This is home page </h1>");
+        res.end();
+    } else if (req.url == "/about") {
+        res.writeHead(200, {
+            "Content-Type": "text-html",
+        });
+        res.write("<h1>This is about page</h1>");
+        res.end();
+    } else if (req.url == "/contact") {
+        res.writeHead(200, {
+            "Content-Type": "text-html",
+        });
+        res.write("<h1>this is contact page </h1>");
+        res.end();
+    }
 });
 
 server.listen(5050);
